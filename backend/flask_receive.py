60e0049a3a8bdd031956c78c7d1e5ffcb49a0e3b
Willyou2 <wcen2009@gmail.com>
2019-02-08 23:38:14 -0500

    Working on Flask

---
+++ backend/flask_receive.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+if(__name__ == __main__) app.run(host='0.0.0.0')
+
+@app.route('/<arg_1>') #Route is denoted by the 
+def hello_world(arg_1):
+	#data = request.data
+	#print(data)
+	return 'Hello, World!' + ' ' + arg_1
+
+@app.route('/')
+def humaness():
+	return 'Intelligence!'
+
